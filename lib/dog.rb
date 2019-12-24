b37898170d6d74eb75bd71b773d59d08a255c1b4
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-24 01:28:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Add your code here
+class Dog
+
+	@@all = []
+	attr_accessor :name
+
+	def initialize(name)
+		@name = name
+		@@all << self
+	end
+
+	def self.clear_all
+		@@all.clear
+
+	end
+
+	def self.all
+		@@all.each do |dog|
+			puts dog.name
+		end
+	end
+end
